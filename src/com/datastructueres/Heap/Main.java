c1c43845ed31368fde991e4b1a979a10ca32d9f7
iiRyan <92063117+iiRyan@users.noreply.github.com>
2024-04-30 07:34:18 +0300

    Solve Two exercises.

--- src/com/datastructueres/Heap/Main.java
+++ src/com/datastructueres/Heap/Main.java
@@ -1,23 +1,18 @@
 package com.datastructueres.Heap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
 
         // Test case 1
-        int[] nums1 = {7, 10, 4, 3, 20, 15};
-        int k1 = 3;
+        int[] nums1 = {1, 5, 2, 9, 3, 6, 8};
         System.out.println("Test case 1:");
-        System.out.println("Expected output: 7");
-        System.out.println("Actual output: " + findKthSmallest(nums1, k1));
-        System.out.println();
-
-
-        int[] nums2 = {2, 1, 3, 5, 6, 4};
-        int k2 = 2;
-        System.out.println("Test case 2:");
-        System.out.println("Expected output: 2");
-        System.out.println("Actual output: " + findKthSmallest(nums2, k2));
+        System.out.println("Input: [1, 5, 2, 9, 3, 6, 8]");
+        System.out.println("Expected output: [1, 5, 5, 9, 9, 9, 9]");
+        System.out.println("Actual output: " + streamMax(nums1));
         System.out.println();
     }
 
@@ -44,4 +39,33 @@ public class Main {
         // Therefore, we remove and return it.
         return maxHeap.remove();
     }
+
+
+
+
+    public static List<Integer> streamMax(int[] nums) {
+        // Create an instance of a max-heap
+        Heap maxHeap = new Heap();
+
+        // Prepare a list to store the max of the stream so far
+        List<Integer> maxStream = new ArrayList<>();
+
+        // For each number in the given array
+        for (int num : nums) {
+            // Insert the current number into the max-heap
+            maxHeap.insert(num);
+
+            // After insertion, the max of the heap (which is also
+            // the max of the stream so far) is at the root. Add this
+            // root to the maxStream list
+            maxStream.add(maxHeap.getHeap().get(0));
+        }
+
+        // After iterating over all the numbers, return the maxStream list
+        // that contains the running maximum of the input stream
+        return maxStream;
+    }
+
+
+
 }
